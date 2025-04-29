71d6ddaa9adb1180f7f1270d4f6746c8f3c26c74
dipanshuu <dipanshusahu447@gmail.com>
2025-04-29 23:32:33 +0530

    header work done

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <header>
+            <div className="head">
+                <h1>Good Morning, John!</h1>
+                <p>Today, Wed 6 July 2023</p>
+            </div>
+            <button><img src="https://img.icons8.com/?size=100&id=120374&format=png&color=000000" alt="menu" /></button>
+        </header>
+    )
+}
