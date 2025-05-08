0a61f53bf4fedae17518a8cd2e712d73e02e3fea
dipanshuu <dipanshusahu447@gmail.com>
2025-05-08 22:40:06 +0530

    Improved handling of create task input

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 export default function CreateTask({addTask}) {
+    const [input, setInput] = useState('');
 
     function handleKey(event) {
-        if(event.key == "Enter"){
-            if(event.target.value.trim() == "") return;
-            addTask(prev => [...prev, event.target.value]);
+        if(event.key == "Enter" && input.trim() !== ""){
+            addTask(prev => [...prev, input]);
+            setInput('');
         }
     }
 
@@ -11,7 +14,7 @@ export default function CreateTask({addTask}) {
         <div className="createTask">
             <div>
                 <img src="https://img.icons8.com/?size=100&id=3220&format=png&color=ffffff" alt="plus-img" />
-                <input type="text" placeholder='Create new task' onKeyDown={handleKey}/>
+                <input type="text" placeholder='Create new task' value={input} onChange={e => setInput(e.target.value)} onKeyDown={handleKey}/>
             </div>
         </div>
     )
