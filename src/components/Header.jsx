52577462dfd8d6b3b478e62a8bd2f8dcfe295f13
dipanshuu <dipanshusahu447@gmail.com>
2025-05-09 00:24:21 +0530

    Auto update date and greetings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-export default function Header(){
+import { useState, useEffect } from 'react';
+
+export default function Header() {
+    let [date, setDate] = useState(() => new Date());
+    let formattedDate = date.toLocaleDateString('en-IN', {
+        weekday: 'short',
+        day: 'numeric',
+        month: 'long',
+    })
+
+    useEffect(() => {
+        const minInterval = setInterval(() => setDate(new Date()), 60 * 1000);
+        return () => clearInterval(minInterval);
+    }, [])
+    
+    let hour = date.getHours();
+    let greeting = hour < 12 ? 'Good Morning' : hour < 17 ? 'Good Afternoon' : 'Good Evening';
+
     return (
         <header>
             <div className="head">
-                <h1>Good Morning, John!</h1>
-                <p>Today, Wed 6 July 2023</p>
+                <h1>{greeting}, John!</h1>
+                <p>Today, {`${formattedDate} ${date.getFullYear()}`}</p>
             </div>
             <button className="header-menu"><img src="https://img.icons8.com/?size=100&id=120374&format=png&color=000000" alt="menu" /></button>
         </header>
