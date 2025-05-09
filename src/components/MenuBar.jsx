d5e0175e320b6d7cf654fecdc874e7da09903612
dipanshuu <dipanshusahu447@gmail.com>
2025-05-09 13:54:36 +0530

    easily updatable clean jsx menu options update

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,3 +1,29 @@
+const menuItem = [
+    {
+        lable: "Welcome",
+        icon: "https://img.icons8.com/?size=100&id=37965&format=png&color=202020"
+    },
+    {
+        lable: "ToDolist",
+        icon: "https://img.icons8.com/?size=100&id=f9MbZqpEUKcQ&format=png&color=202020"
+    },
+    {
+        lable: "Pomodoro",
+        icon: "https://img.icons8.com/?size=100&id=ggkq7ZKoggie&format=png&color=202020"
+    },
+    {
+        lable: "Coming Soon",
+        icon: "https://img.icons8.com/?size=100&id=ytYtK65QNJyO&format=png&color=202020"
+    }
+]
+
+let menuOptions = menuItem.map((item, index) => (
+    <li key={index}>
+        <img src={item.icon} alt={item.lable + " option"} />
+        {item.lable}
+    </li>
+))
+
 export default function Menu() {
     return (
         <div className="menu">
@@ -6,22 +32,7 @@ export default function Menu() {
             </div>
             <div className="menu-options">
                 <ul>
-                    <li>    
-                        <img src="https://img.icons8.com/?size=100&id=37965&format=png&color=202020" alt="" />
-                        Welcome
-                    </li>
-                    <li>
-                        <img src="https://img.icons8.com/?size=100&id=f9MbZqpEUKcQ&format=png&color=202020" alt="" />
-                        ToDolist
-                    </li>
-                    <li>
-                        <img src="https://img.icons8.com/?size=100&id=ggkq7ZKoggie&format=png&color=202020" alt="" />
-                        Pomodoro
-                    </li>
-                    <li>
-                        <img src="https://img.icons8.com/?size=100&id=ytYtK65QNJyO&format=png&color=202020" alt="" />
-                        Coming Soon
-                    </li>
+                    {menuOptions}
                 </ul>
             </div>
         </div>
