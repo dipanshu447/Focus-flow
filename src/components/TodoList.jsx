a417be98217eb737010047a726fea70b4078c019
dipanshuu <dipanshusahu447@gmail.com>
2025-05-12 15:27:56 +0530

    Add task dialog box feature

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,44 +1,66 @@
 import Header from './Header.jsx';
-import CreateTask from './todolistComponents/CreateTask.jsx';
 import ProgressBar from './todolistComponents/ProgressBar.jsx';
+import { useState } from 'react';
 
-export default function TodoList({addTask, taskDoneLength, taskLength,remainingTaskCount, userTasks}) {
+export default function TodoList({ addTask, taskDoneLength, taskLength, remainingTaskCount, userTasks }) {
 
     let taskMessage = taskLength == 0 ? "No tasks yet. Add one to get started!" : (taskDoneLength - remainingTaskCount) == 0 ? "You're all caught up. Great job!" : `${taskDoneLength - remainingTaskCount} Tasks Remaining.`;
 
     let taskProgress = (remainingTaskCount / taskDoneLength) * 100;
     let progressPercent = isNaN(taskProgress) ? 0 : Math.round(taskProgress);
 
+    function handleInput(e){
+       e.preventDefault();
+       let data = new FormData(e.target); 
+       let taskInput = data.get('task');
+       addTask(taskInput);
+       e.target.reset();
+    }
+
+    const [showAddTask, setshowAddTask] = useState(false);
+    function toggleShowAddTask() {
+        setshowAddTask(prev => !prev);
+    }
+
     return (
-        <div className="todoList">
-            <div className='main'>
-                <Header />
-                <div className="subHead">
-                    <div className="leftOver">{taskMessage}</div>
-                    <div className="status">
-                        <small>{progressPercent}%</small>
-                        <ProgressBar 
-                            strokeSize='5'
-                            CircleSize='35'
-                            progress={progressPercent} 
-                        />
+        <>
+            <div className="todoList" style={showAddTask ? {filter:"blur(10px) brightness(0.9)"} : {}}>
+                <div className='main'>
+                    <Header />
+                    <div className="subHead">
+                        <div className="leftOver">{taskMessage}</div>
+                        <div className="status">
+                            <small>{progressPercent}%</small>
+                            <ProgressBar
+                                strokeSize='5'
+                                CircleSize='35'
+                                progress={progressPercent}
+                            />
+                        </div>
                     </div>
+                    <div className="options">
+                        <button className='newTask' onClick={toggleShowAddTask}>
+                            <img src="https://img.icons8.com/?size=100&id=3220&format=png&color=ffffff" alt="plusIcon" />
+                            New Task
+                        </button>
+                        {/* <button className='filter'>
+                            <img src="https://img.icons8.com/?size=100&id=10751&format=png&color=000000" alt="plusIcon" />
+                            Filters
+                        </button> */}
+                    </div>
+                    <section>
+                        {userTasks}
+                    </section>
                 </div>
-                <div className="options">
-                    <button className='newTask'>
-                        <img src="https://img.icons8.com/?size=100&id=3220&format=png&color=ffffff" alt="plusIcon" />
-                        New Task
-                    </button>
-                    <button className='filter'>
-                        <img src="https://img.icons8.com/?size=100&id=10751&format=png&color=000000" alt="plusIcon" />
-                        Filters
-                    </button>
-                </div>
-                <section>
-                    {userTasks}
-                </section>
             </div>
-            {/* <CreateTask addTask={addTask} /> */}
-        </div>
+            {showAddTask && <form className="taskDialogBox" onSubmit={handleInput}>
+                <label>Add new Task</label>
+                <input type="text" name='task'/>
+                <div className="btns">
+                    <button type='button' onClick={toggleShowAddTask}>Back</button>
+                    <button type='submit'>Add</button>
+                </div>
+            </form>}
+        </>
     )
 }
