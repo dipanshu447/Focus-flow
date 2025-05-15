ac7f5ce9829cfb6d3b5dd4ee6de2c62b8b9e9314
dipanshu447 <dipanshusahu447@gmail.com>
2025-05-15 20:51:54 +0530

    Changes left for later

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function Header({username}) {
+export default function Header({ username }) {
     let [date, setDate] = useState(() => new Date());
     let formattedDate = date.toLocaleDateString('en-IN', {
         weekday: 'short',
@@ -12,7 +12,7 @@ export default function Header({username}) {
         const minInterval = setInterval(() => setDate(new Date()), 60 * 1000);
         return () => clearInterval(minInterval);
     }, [])
-    
+
     let hour = date.getHours();
     let greeting = hour < 12 ? 'Good Morning' : hour < 17 ? 'Good Afternoon' : 'Good Evening';
 
@@ -22,7 +22,10 @@ export default function Header({username}) {
                 <h1>{greeting}, <span>{username}!</span></h1>
                 <p>Today, {`${formattedDate} ${date.getFullYear()}`}</p>
             </div>
-            <button className="header-menu"><img src="https://img.icons8.com/?size=100&id=120374&format=png&color=000000" alt="menu" /></button>
+            {/* <label class="switch">
+                <input type="checkbox" />
+                <span class="slider"></span>
+            </label> */}
         </header>
     )
 }
