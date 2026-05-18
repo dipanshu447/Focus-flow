fc1990832b2ffbcfccff3d06fc4cbdc283bf108a
dipanshu447 <dipanshusahu447@gmail.com>
2026-05-18 18:11:17 +0530

    Revert "feature: connected backend and frontend for contact form" This reverts commit d690950486bd2c4a5534c7733ee7f0f7270150fe.

--- backend/src/validators/contact.validator.js
+++ backend/src/validators/contact.validator.js
@@ -1,7 +1,7 @@
 import {z} from 'zod';
 
 export const contactSchema = z.object({
-    name: z.string().min(2, "Name is too short").max(20, "Name is too long"),
+    name: z.string().min(2, "Name is too short").max(10, "Name is too long"),
     email: z.string().email("Invalid email address"),
     message: z.string().min(10, "Message is too short").max(1000, "Message is too long")
 })
